b36e088dedd32eef8ba27d20facb716551e36085
Paul Schaack <paul.schaack2@gmail.com>
2013-10-22 01:27:01 +0100

    stripe and flask

--- payme/__init__.py
+++ payme/__init__.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask
+
+stripe_keys = {
+    'secret_key': os.environ['SECRET_KEY'],
+    'publishable_key': os.environ['PUBLISHABLE_KEY']
+}
+
 app = Flask(__name__)
 
-
 import payme.views
+
+
--- payme/views.py
+++ payme/views.py
@@ -1,10 +1,30 @@
-from payme import app
+from payme import app, stripe_keys
+from flask import render_template, request
+import stripe
 
+stripe.api_key = stripe_keys['secret_key']
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', key=stripe_keys['publishable_key'])
 
+@app.route('/charge', methods=['POST'])
+def charge():
+    # Amount in cents
+    amount = 5000
+
+    customer = stripe.Customer.create(
+        email='customer@example.com',
+        card=request.form['stripeToken']
+    )
+
+    charge = stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='gbp',
+        description='credit to cash'
+    )
+    return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
     app.run()
