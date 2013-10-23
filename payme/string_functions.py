d125cc8dfb246b86495d8c7330101f61a66e9e99
Paul Schaack <paul.schaack2@gmail.com>
2013-10-23 12:58:34 +0100

    completed string validations and conversions

--- payme/string_functions.py
+++ payme/string_functions.py
@@ -11,8 +11,29 @@ def matches_reg_ex(input_string, reg_ex):
     and if it matches the regular expression.
     Empty strings return False.
     """
-    return (isinstance(input_string, basestring) and
-            re.match(reg_ex, input_string) != None)
+    return (isinstance(input_string, basestring)
+            and re.match(reg_ex, input_string) != None)
+
+def valid_name(name):
+    """
+    Checks if account holder name is valid and a string.
+    Valid: 1-18 characters long
+    """
+    reg_ex = "^[A-Za-z0-9 _-]+$"
+    length = 18
+    return (matches_reg_ex(name, reg_ex)
+            and len(name)<=length)
+
+def valid_reference(reference):
+    """
+    Checks if reference is valid and a string.
+    Valid: 0-18 characters long
+    """
+    if (isinstance(reference, basestring)
+        and len(reference)==0):
+        return True
+    else:
+        return valid_name(reference)
 
 def valid_account_number(account_number):
     """
@@ -33,9 +54,9 @@ def valid_sort_code(sort_code):
     return (matches_reg_ex(sort_code, reg_ex)
             and len(sort_code)==length)
 
-def alter_sort_code(sort_code):
+def convert_sort_code(sort_code):
     """
-    Transforms common mistakes in sort code to correct format
+    Converts common mistakes in sort code to correct format
     """
     # 12 34.56 -> 12-34-56, 12_34:45 -> 12-34-56, etc.
     reg_ex = "[0-9][0-9][ _:.-][0-9][0-9][ _:.-][0-9][0-9]"
@@ -52,13 +73,22 @@ def alter_sort_code(sort_code):
         return (sort_code[0:2] + '-'
                 + sort_code[2:4] + '-'
                 + sort_code[4:6])
-
     return sort_code
 
-def convert_whitespace(input_string):
-    return string.replace(input_string, ' ', '_')
+def convert_special_characters(input_string):
+    input_string = string.replace(input_string, ' ', '_')
+    input_string = string.replace(input_string, '"', '_')
+    input_string = string.replace(input_string, "'", "_")
+    input_string = string.replace(input_string, '#', '_')
+    input_string = string.replace(input_string, '<', '_')
+    input_string = string.replace(input_string, '>', '_')
+    input_string = string.replace(input_string, '\t', '_')
+    input_string = string.replace(input_string, '\n', '_')
+    input_string = string.replace(input_string, '/', '_')
+    input_string = string.replace(input_string, '\\', '_')
+    return input_string
 
-def convert_comma(input_string):
+def convert_price(input_string):
     return string.replace(input_string, ',', '.')
 
 def valid_price(price):
@@ -68,19 +98,20 @@ def valid_price(price):
     reg_ex = "^[0-9]+[.][0-9][0-9]$"
     return matches_reg_ex(price, reg_ex)
 
-def into_pence(price):
+def price_in_pence(price):
     """
     129.99 -> 12999
     """
     if valid_price(price):
         return str(int(float(price) * 100.0))
 
-def into_pound(price):
+def price_in_pound(price):
     """
     123 -> 1.23
     """
     if is_integer_string(price):
         return str(int(price) / 100.0)
+
         
 
 
